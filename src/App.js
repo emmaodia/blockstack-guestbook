8283921af496c7c4bfc7e82b57e9d020f3b8c7cd
emmaodia <odia.emma@gmail.com>
2020-06-28 18:17:26 +0100

    Working on App routing Navigation.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import React from 'react';
-import Header from './Components/Header';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Register from './Components/Register';
 import { appCpnfig } from './assets/constants';
 import { Connect } from '@blockstack/connect';
+import ReactBlockstack, { useBlockstack, didConnect, useFile } from 'react-blockstack';
+import { appConfig } from './assets/constants';
 
-const authOptions = {
-  redirectTo: '/',
-  finished: ({ userSession }) => {
-    console.log(userSession.loadUserData());
-  },
-  appDetails: {
-    name: 'BlockStack Guest Book',
-    icon: window.location.origin + '/register.svg',
-  },
-};
+ReactBlockstack({ appConfig });
 
-function App() {
+const App = props => {
+
+  const { showOnBoard } = props;
+
+  const authOptions = {
+    redirectTo: '/',
+    finished: ({ userSession }) => {
+      console.log(userSession.loadUserData());
+    },
+    appDetails: {
+      name: 'BlockStack Guest Book',
+      icon: window.location.origin + '/register.svg',
+    },
+  };
+
   return (
     <>
       <Connect authOptions={authOptions}>
-        <Header />
         <div className="flex flex-wrap mt-20">
-          <Register />
-        </div>
-        <div>Welcome...</div>
+          <Register>
+            <div>Welcome...</div>
+            <Switch>
+              <Route path="/register" component={Register}>
+              {showOnBoard && <Redirect to="/register" />}
+              </Route>
+            </Switch>
+          </Register>
+        </div>       
       </Connect>
     </>
   );
