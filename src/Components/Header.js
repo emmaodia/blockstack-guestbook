025e644c12c34e5c5e4376f87f34b2a7b5842e12
emmaodia <odia.emma@gmail.com>
2020-06-28 18:18:31 +0100

    Register and Sign Up completed.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -39,7 +39,7 @@ const Header = () => {
               </a>
             </div>
             <div>
-            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            <a href="#responsive-header" onClick={onClick} className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                 Sign In
               </a>
               <button onClick={onClick} className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Register</button>
