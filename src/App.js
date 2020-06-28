30c94578040a530c6ee78c06ce21c1935c90767b
emmaodia <odia.emma@gmail.com>
2020-06-28 15:54:44 +0100

    Updated App.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
 import Header from './Components/Header';
+import Register from './Components/Register';
+import { appCpnfig } from './assets/constants';
+import { Connect } from '@blockstack/connect';
+
+const authOptions = {
+  redirectTo: '/',
+  finished: ({ userSession }) => {
+    console.log(userSession.loadUserData());
+  },
+  appDetails: {
+    name: 'BlockStack Guest Book',
+    icon: window.location.origin + '/register.svg',
+  },
+};
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Connect authOptions={authOptions}>
         <Header />
-      </BrowserRouter>
-     <div>Welcome...</div>
+        <div className="flex flex-wrap mt-20">
+          <Register />
+        </div>
+        <div>Welcome...</div>
+      </Connect>
     </>
   );
 }
