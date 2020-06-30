a7fd17f534214618ba981ded73f9f5bb39123bd4
emmaodia <odia.emma@gmail.com>
2020-06-30 23:57:19 +0100

    Removed commented code.

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -20,10 +20,8 @@ const SignIn = () => {
       <nav className="fixed w-full z-10 top-0 flex items-center justify-between flex-wrap bg-teal-500 p-6">
           <div className="flex items-center flex-shrink-0 text-white mr-6">
             <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg">
-            {element}
-              {/* <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/> */}
-              </svg>
-            {/* {element} */}
+              {element}
+            </svg>
             <span className="font-semibold text-xl tracking-tight">
               <Link to='/'>Guest Book</Link>
             </span>
@@ -49,34 +47,3 @@ const SignIn = () => {
 }
 
 export default SignIn;
-
-
- 
-//  <div style={HeaderStyle}>
-//  <Navbar color="light" light expand="md">
-//    <NavbarBrand>Home</NavbarBrand>
-//    <NavbarToggler onClick={toggle} />
-//    <Collapse isOpen={isOpen} navbar>
-//      <Nav className="mr-auto" navbar>
-//        <NavItem>
-//          <NavLink href="/components/">Projects</NavLink>
-//        </NavItem>
-//        <NavItem>
-//          <NavLink href="https://github.com/emmaodia/contributetoit">GitHub</NavLink>
-//        </NavItem>
-//      </Nav>
-//      <Button color="secondary">Creat Project</Button>
-//      {/* <NavbarText>Creat Project</NavbarText> */}
-//    </Collapse>
-//  </Navbar>
-//   <Switch>
-//      {routes.map(({ path, component: Component, exact}, index ) => (
-//        <Route 
-//          key={index}
-//          path={path}
-//          exact={exact}
-//          render={(props) => <Component {...props} />}
-//        />
-//      ))}
-//    </Switch>
-// </div>
