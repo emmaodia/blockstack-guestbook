a0aa89f9f2673aabe48bd797f79f969b15a48c2a
emmaodia <odia.emma@gmail.com>
2020-06-30 23:57:09 +0100

    Removed commented code.

--- src/Components/GuestBook.js
+++ src/Components/GuestBook.js
@@ -12,12 +12,11 @@ function Layout () {
 
     return (
         <>
-        <nav className="fixed w-full z-10 top-0 flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <nav className="fixed w-full z-10 top-0 flex items-center justify-between flex-wrap bg-gray-500 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
               <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg">
-              {icon}
-                {/* <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/> */}
-                </svg>
+                {icon}
+              </svg>
               {/* {element} */}
               <span className="font-semibold text-xl tracking-tight">
                 <Link to='/'>Guest Book</Link>
